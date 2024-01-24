092fff4a06562eb6d3b9dc310e8a342318199389
Daniela <martinezdl_@hotmail.com>
2024-01-24 16:35:22 -0300

    import new compnents

--- app/page.jsx
+++ app/page.jsx
@@ -1,38 +1,43 @@
 import Link from "next/link";
 import prisma from "../lib/prisma";
 import Post from "./components/Post";
+import Article from "./components/Article";
+import Footer from "./components/Footer";
 
- async function getPosts(){
-    const posts = await prisma.post.findMany({
-      where: {published: true},
-      include: {
-        author: {
-          select: {name: true}
-        }
-      }
-    })
-    return posts;
-  }
-export default  async function Home() {
- const posts=await getPosts()
- console.log("posts",posts)
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: {
+        select: { name: true },
+      },
+    },
+  });
+  return posts;
+}
+export default async function Home() {
+  const posts = await getPosts();
+  //  console.log("posts",posts)
   return (
-    <main className="bg-yellow-200">
-      <Link href={'/add-post'} className="text-black font-bold">Add Post</Link>
-      <h1>Feed</h1>
-      {
-        posts.map((post) => {
+    <main className="flex flex-col max-w-screen-lg mx-auto">
+      <Article />
+      <div className="border-t mt-12 pt-12 pb-32 px-4 lg:px-0">
+        <Link href={"/add-post"} className="text-lg  my-6">
+          Post a comment:
+        </Link>
+        {posts.map((post) => {
           return (
             <Post
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            content={post.content}
-            authorName={post.author.name}
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              authorName={post.author.name}
             />
-          )
-        })
-      }
+          );
+        })}
+      </div>
+      <Footer />
     </main>
   );
 }

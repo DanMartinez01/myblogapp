fe92080fa74aabcc78807e998c407df8e5fbb85e
Daniela <martinezdl_@hotmail.com>
2024-01-24 16:31:42 -0300

    change styles in Post component

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -1,12 +1,29 @@
-// import DeletePostButton from "./DeletePostButton";
+import DeleteBtn from "../components/DeleteBtn";
+import Image from "next/image";
+import capybara from "../../public/capybara.jpeg";
 
-export default function Post({id, title, content, authorName}){
-    return (
-        <div style={{border: '1px solid white', padding: '15px', margin: '10px 0px'}}>
-            <h3>{authorName}</h3>
-            <h4>{title}</h4>
-            <p>{content}</p>
-            {/* <DeletePostButton postId={id} /> */}
+export default function Post({ id, title, content, authorName }) {
+  return (
+    <div className="max-w-screen-sm w-2/4 my-2">
+      <div className="space-y-4">
+        <div className="flex">
+          <div className="flex-shrink-0 mr-3">
+            <Image
+              className="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10"
+              src={capybara}
+              alt=""
+              width="100"
+              height="100"
+            />
+          </div>
+          <div className="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed">
+            <strong>{authorName}</strong>{" "}
+            <span className="text-xs text-gray-400">3:34 PM</span>
+            <p className="text-sm w-100">{content}</p>
+            <DeleteBtn postId={id} />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }

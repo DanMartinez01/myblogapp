b47028d1b35b422b0687275efeb0ac38b5faf1e0
Daniela <martinezdl_@hotmail.com>
2024-01-24 22:42:19 -0300

    add cache-control

--- app/api/post/[id]/route.js
+++ app/api/post/[id]/route.js
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function DELETE(request, { params }) {
+export async function DELETE(request, res, { params }) {
   const id = params.id;
+  res.setHeader("Cache-Control", "no-store");
   const post = await prisma.post.delete({
     where: { id },
   });

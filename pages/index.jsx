73b79083dab7749dd8babb02b27ae0905bb0e6b6
Daniela <martinezdl_@hotmail.com>
2024-02-08 22:19:10 -0300

    add Inputs component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Link from "next/link";
 import prisma from "../lib/prisma";
 import Post from "../app/components/Post";
@@ -5,6 +6,7 @@ import Form from "@/app/components/Form";
 import Article from "../app/components/Article";
 import Footer from "../app/components/Footer";
 import Navbar from "@/app/components/Navbar";
+import Inputs from "@/app/components/Inputs";
 // Example for importing global styles
 import "../styles/globals.css";
 
@@ -26,19 +28,21 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ posts }) {
+  // useEffect(() => {
+  //   const sectionElement = document.getElementById("posts");
+
+  //   if (sectionElement) {
+  //     window.scrollTo(0, sectionElement.offsetTop);
+  //   }
+  // }, []);
   console.log(posts);
   return (
     <main className="flex flex-col max-w-screen-lg mx-auto">
       <Navbar />
       <Article />
-      <div className="border-t mt-10 pt-12 pb-32 px-4 lg:px-0">
-        <Link
-          href={"/add-post"}
-          className="border-b-2 border-white hover:text-indigo-600 transition-colors hover:border-indigo-600 "
-        >
-          Post your comment:
-        </Link>
-        {/* <Form /> */}
+      <div className="border-t mt-10 pt-12 pb-32 px-4 lg:px-0" id="posts">
+        <h1>Post your comment:</h1>
+        <Inputs />
         {posts.map((post) => (
           <Post
             key={post.id}

044bfccb8dc2907b963e18af6df0b53be0f3403f
Daniela <martinezdl_@hotmail.com>
2024-02-08 22:16:14 -0300

    change footer links

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="border-t mt-12 pt-12 pb-32 px-4 lg:px-0">
+    <footer className="border-t mt-12 pt-4 pb-6 px-4 lg:px-0">
       <div className="flex flex-wrap">
         <div className="w-full mt-6 lg:mt-0 md:w-1/2 lg:w-1/5">
-          <ul>
+          <ul className="flex flex-row justify-between ">
             <li>
               {" "}
               <a href="" className="block text-gray-600 py-2">
@@ -16,20 +16,10 @@ const Footer = () => {
                 Github
               </a>{" "}
             </li>
-          </ul>
-        </div>
-        <div className="w-full mt-6 lg:mt-0 md:w-1/2 lg:w-1/5">
-          <ul>
             <li>
               {" "}
               <a href="" className="block text-gray-600 py-2">
                 Portfolio
-              </a>{" "}
-            </li>
-            <li>
-              {" "}
-              <a href="" className="block text-gray-600 py-2">
-                Instagram
               </a>{" "}
             </li>
           </ul>
